f08d1bc5268d09f2ed539f6dcca5fabeae2fe401
Rocky Luvis <rockyinott24@yahoo.com>
2024-08-03 13:03:20 -0500

    changes for MLOps project

---
+++ init_setup.sh
@@ -0,0 +1,15 @@
+echo [$(date)]: "START"
+
+echo [$(date)]: "Creating env with Python 3.8 v"
+
+conda create --prefix ./env python=3.8 -y
+
+echo [$(date)]: "Activating the environment"
+
+source activate ./env
+
+echo [$(date)] : "installing dev requirements"
+
+pip install -r requirements.txt
+
+echo [$(date)]: "END"
