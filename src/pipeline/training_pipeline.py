c5102b67c376bf2849867e2eaa08e653a20638a9
Rocky Luvis <rockyinott24@yahoo.com>
2024-08-07 16:06:56 -0500

    ML Flow added

--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -10,6 +10,7 @@ import numpy as np
 from src.components.data_ingestion import DataIngestion
 from src.components.data_transformation import DataTransformation
 from src.components.model_trainer import ModelTrainer
+from src.components.model_evaluation import ModelEvaluation
 
 obj = DataIngestion()
 
@@ -19,5 +20,10 @@ data_transformation = DataTransformation()
 
 train_arr, test_arr= data_transformation.initialize_data_transformation (train_data_path, test_data_path)
 
+# Train the Model
 model_trainer_obj = ModelTrainer()
 model_trainer_obj.initiate_model_training(train_arr, test_arr)
+
+#Evaluate the Model
+model_eval_obj = ModelEvaluation()
+model_eval_obj.iniate_model_evaluation(train_arr, test_arr)
