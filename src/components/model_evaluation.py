00e780aababe908161e33a54f4718d6066b89782
Rocky Luvis <rockyinott24@yahoo.com>
2024-08-07 16:06:56 -0500

    ML Flow added

--- src/components/model_evaluation.py
+++ src/components/model_evaluation.py
@@ -1,17 +1,19 @@
 import os
 import sys
 
-from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from urllib.parse import urlparse
 
 from src.logger.logging import logging
-from src. exception.exception import CustomException
+from src.exception.exception import CustomException
 
 import mlflow
 import mlflow.sklearn
 import numpy as np
 import pickle
-#from src.D
+
+from src.utils.utils import load_object 
+
 
 # Data ingestion -> Data Transformation -> Model build/trainer -> model evaluation
 # In components we have Configuation and Artifact , Artifact is the output of the component
@@ -20,17 +22,60 @@ import pickle
 
 
 class ModelEvaluationConfig:
+
     pass
 
 class ModelEvaluation():
 
     def __init__(self):
-        pass
+        logging.info("Starting Evaluation of the model....")
 
-    def iniate_model_evaluation(self):
+    def eval_metrics(self, actual, pred):
+        rmse = np.sqrt(mean_squared_error(actual, pred))
+        mae = mean_absolute_error(actual, pred)
+        r2 = r2_score (actual, pred)
+
+        logging.info(f"C evaluation metrics {rmse}, {mae}, {r2} - rmse, mae, r2")
+        return rmse, mae, r2
+
+
+    def iniate_model_evaluation(self, train_array, test_array):
         try:
-            pass
+            #Segreegate Test and train data.
+            x_test, y_test = (test_array[:,:-1],
+                              test_array[:,-1])
+            x_train, y_train = (train_array[:,:-1],
+                                train_array[:,-1]
+                                )
+            
+            model_path = os.path.join ("artifacts", "model.pkl")
+            model = load_object(model_path)
+
+            #Specify path where to register the Model
+            #pass the url here
+            mlflow.set_registry_uri("")
+            logging.info(" Model has been registred ")
+
+            tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+            print(tracking_url_type_store)
+
+            with mlflow.start_run():
+                prediction = model.predict(x_test)
+                (rmse, mae, r2) = self.eval_metrics(y_test, prediction)
+                mlflow.log_metric("rmse", rmse)
+                mlflow.log_metric("mae", mae)
+                mlflow.log_metric("r2",r2)
+
+                #if we logging in local and not in a registry_uri then use this condition
+                if tracking_url_type_store != "file":
+
+                    #Register the model
+                    #If cloud location is avilable log in cloud location. 
+                    mlflow.sklearn.log_model(model, "model", registered_model_name="ml_model")
+                else:
+                    #Log locally
+                    mlflow.sklearn.log_model(model, "model")
 
         except Exception as e:
-            logging.info()
+            logging.info("Exception in Model Evaluation")
             raise CustomException (e,sys)
